5709c84244ba9c5fb5db47726cc691e4e0bccb24
Mike Walston <mike@mikewalston.com>
2020-09-02 17:27:05 +0900

    Commit of initial main.py implementation and vgg.py

---
+++ main.py
@@ -0,0 +1,115 @@
+from __future__ import print_function
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+from vgg import VggNet
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+net = VggNet(in_channels=3, num_classes=10, size=32, vgg_type='VGG11')
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+# show images on the plot
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+    print('started epoch', epoch)
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 10 == 9:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+
+print('Finished Training')
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+net.load_state_dict(torch.load(PATH))
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
---
+++ vgg.py
@@ -0,0 +1,105 @@
+# Implementation of VGG16 as described in https://arxiv.org/pdf/1409.1556
+# "VERY DEEP CONVOLUTIONAL NETWORKS FOR LARGE-SCALE IMAGE RECOGNITION"
+#
+# also based on the code-along tutorial at https://youtu.be/ACmuBbuXn20 but
+# with additional sourcing back to the original paper and explanations to
+# myself for reference
+import torch
+import torch.nn as nn
+
+# Convolutional layers
+# Integer value means outputs, M is for maxpool
+VGG = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+}
+
+# constants as defined in the paper
+# p.2 2.1
+conv_kernel_size = (3, 3)
+conv_stride = (1, 1)
+conv_padding = (1, 1)
+max_pool_kernel_size = (2, 2)
+max_pool_stride = (2, 2)
+
+
+class VggNet(nn.Module):
+    def __init__(self, in_channels=3, num_classes=1000, size=224, vgg_type='VGG16'):
+        super(VggNet, self).__init__()
+        self.in_channels = in_channels
+        self.convolution_layers = self.create_conv_layers(VGG[vgg_type])
+
+        self.fully_connected_layers = nn.Sequential(
+
+            # 512 because 512 is the number of outputs of the last
+            # convolutional layer defined for VGG16
+            # 7 because we have 5 maxpools so 224 / (2 ^ 5) = 7
+            # 4096 because that is what is stated in the paper
+            # p.3 Table 1
+            nn.Linear(int(512 * (size / 32)**2), 4096),
+
+            # p.2 2.1  (ReLU is short for rectified linear unit, an activation function)
+            nn.ReLU(),
+
+            # p.4 3.1 (Dropout is for reducing overfit data)
+            nn.Dropout(p=0.5),
+
+            # second Linear layer with 4096 input to match 4096 outputs of first linear layer
+            nn.Linear(4096, 4096),
+
+            # p.2 2.1
+            nn.ReLU(),
+
+            # p.4 3.1
+            nn.Dropout(p=0.5),
+
+            # third Linear layer with 4096 input to match 4096 outputs of first linear layer
+            # and the number of outputs to equal the number of classes we have
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        # call the convolution layers
+        # "shape after convolution layers should be 1 x 3 x 244 x 244
+        x = self.convolution_layers(x)
+        # reshape the convolutional layers into a linear structure (1 dimension) for
+        # processing by the nn.Sequential code
+        x = x.reshape(x.shape[0], -1)
+        # shape after reshape should be 1 x 25088
+        # call our fully connected layer code with the newly reshaped x
+        x = self.fully_connected_layers(x)
+        return x
+
+    def create_conv_layers(self, architectures):
+        layers = []
+        # start with in_channels as the initial input channel size from
+        # the class creation, in this case, 3
+        in_channels = self.in_channels
+
+        for x in architectures:
+            if type(x) == int:
+                out_channels = x
+                print(in_channels, out_channels)
+                # create the convolution layers with input and
+                # output channel counts matching our current needs
+                # along with a stride and padding as defined in the paper
+                #
+                # ReLU applied to all convolution layers
+                layers += [nn.Conv2d(in_channels=in_channels,
+                                     out_channels=out_channels,
+                                     kernel_size=conv_kernel_size,
+                                     stride=conv_stride,
+                                     padding=conv_padding),
+                           nn.ReLU()]
+
+                # the input channels for the next layer need to match the output channel
+                # count of the previous layer
+                in_channels = x
+            elif x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=max_pool_kernel_size, stride=max_pool_stride)]
+        return nn.Sequential(*layers)
+
+
+#model = VggNet(in_channels=3, num_classes=1000, size=32)
+#a = torch.randn(1, 3, 32, 32)
+#print(model(a).shape)
